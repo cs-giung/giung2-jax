2929c38e93daa938d47ab2893bd1e8e6eeec3ee8
cs-giung <50647481+cs-giung@users.noreply.github.com>
2022-02-06 04:58:44 +0000

    Update projects/Baseline-ImageNet.

---
+++ projects/Baseline-ImageNet/scripts/eval.py
@@ -0,0 +1,139 @@
+import os
+import sys
+import argparse
+import functools
+from tqdm import tqdm
+sys.path.append('./')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # disable TF logs
+
+import jax
+import jax.numpy as jnp
+from jax.scipy.optimize import minimize
+from flax import jax_utils
+from flax.training import checkpoints
+from flax.training.common_utils import onehot
+
+from giung2.config import get_cfg
+from giung2.data.build import build_dataloaders
+from giung2.modeling.build import build_model
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file', default=None, required=True, metavar='FILE',
+                        help='path to config file')
+    parser.add_argument('--weight_file', default=None, required=True, metavar='FILE',
+                        help='path to weight file')
+    parser.add_argument('--batch_size', default=100, type=int,
+                        help='number of examples per one mini-batch')
+    parser.add_argument('opts', default=None, nargs=argparse.REMAINDER,
+                        help='modify config options at the end of the command')
+    args = parser.parse_args()
+
+    # initialize configuration
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file, allow_unsafe=True)
+    cfg.merge_from_list(args.opts)
+
+    # build model
+    model = build_model(cfg)
+    rng = jax.random.PRNGKey(0)
+
+    # initialize model    
+    def initialize_model(key, model, im_shape, im_dtype):
+        @jax.jit
+        def init(*args):
+            return model.init(*args)
+        var_dict = init({'params': key}, jnp.ones(im_shape, im_dtype))
+        return var_dict
+
+    if cfg.DATASETS.NAME in ['TinyImageNet200',]:
+        image_shape = (1, 64, 64, 3,)
+        num_classes = 200
+    elif cfg.DATASETS.NAME in ['ImageNet1k',]:
+        image_shape = (1, 224, 224, 3,)
+        num_classes = 1000
+    else:
+        raise NotImplementedError
+
+    im_dtype = jnp.float32
+    var_dict = initialize_model(rng, model, image_shape, im_dtype)
+
+    # build dataset
+    dataloaders = build_dataloaders(cfg, batch_size=args.batch_size)
+
+    # load pre-trained weights
+    ckpt = checkpoints.restore_checkpoint(args.weight_file, target=None)
+    var_dict = {
+        'params': ckpt['params'],
+        'image_stats': ckpt['image_stats'],
+        'batch_stats': ckpt['batch_stats'],
+    }
+
+    # make predictions
+    CPU = jax.devices("cpu")[0]
+
+    def predict(var_dict, images):
+        _, outputs = model.apply(var_dict, images, mutable='intermediates')
+        return outputs['intermediates']['classifier']['log_confidences'][0]
+
+    predict = jax.pmap(functools.partial(predict, var_dict), axis_name='batch')
+
+    def make_predictions(dataloader, desc):
+        true_labels, pred_lconfs = [], []
+        for batch in tqdm(dataloader, desc=desc, leave=False):
+            labels = jax.device_put(jnp.concatenate(        batch['labels'] ), CPU)
+            lconfs = jax.device_put(jnp.concatenate(predict(batch['images'])), CPU)
+            true_labels.append(labels)
+            pred_lconfs.append(lconfs)
+        return jnp.concatenate(true_labels), jnp.concatenate(pred_lconfs)
+
+    trn_true_labels, trn_pred_lconfs = make_predictions(
+        jax_utils.prefetch_to_device(dataloaders['trn_loader'](rng=None), size=2),
+        'Make predictions on train examples')
+    val_true_labels, val_pred_lconfs = make_predictions(
+        jax_utils.prefetch_to_device(dataloaders['val_loader'](rng=None), size=2),
+        'Make predictions on valid examples')
+    tst_true_labels, tst_pred_lconfs = make_predictions(
+        jax_utils.prefetch_to_device(dataloaders['tst_loader'](rng=None), size=2),
+        'Make predictions on test examples')
+    trn_confidences = jnp.exp(trn_pred_lconfs)
+    val_confidences = jnp.exp(val_pred_lconfs)
+    tst_confidences = jnp.exp(tst_pred_lconfs)
+
+    # evaluate
+    @jax.jit
+    def evaluate_acc(confidences, true_labels):
+        return jnp.mean(jnp.argmax(confidences, axis=1) == true_labels)
+
+    @jax.jit
+    def evaluate_nll(confidences, true_labels):
+        return jnp.mean(-jnp.sum(jnp.log(confidences) * onehot(true_labels, num_classes=confidences.shape[1]), axis=-1))
+
+    @jax.jit
+    def temperature_scaling(confidences, temperature):
+        return jax.nn.softmax(jnp.log(confidences) / temperature, axis=-1)
+
+    @jax.jit
+    def get_optimal_temperature(confidences, true_labels):
+        def obj(t):
+            return evaluate_nll(temperature_scaling(confidences, t), true_labels)
+        optimal_temperature = minimize(obj, jnp.asarray([1.0,]), method='BFGS', tol=1e-3).x[0]
+        return optimal_temperature
+
+    t_opt = get_optimal_temperature(val_confidences, val_true_labels)
+    print('| Train ACC / NLL / cNLL | Valid ACC / NLL / cNLL | Test ACC / NLL / cNLL  |')
+    print(
+        '| ' +
+        ('%.2f' % (100 * evaluate_acc(                    trn_confidences,         trn_true_labels)))[:5] + ' / ' +
+        ('%.3f' % (      evaluate_nll(                    trn_confidences,         trn_true_labels)))[:5] + ' / ' +
+        ('%.3f' % (      evaluate_nll(temperature_scaling(trn_confidences, t_opt), trn_true_labels)))[:5] + '  | ' +
+        ('%.2f' % (100 * evaluate_acc(                    val_confidences,         val_true_labels)))[:5] + ' / ' +
+        ('%.3f' % (      evaluate_nll(                    val_confidences,         val_true_labels)))[:5] + ' / ' +
+        ('%.3f' % (      evaluate_nll(temperature_scaling(val_confidences, t_opt), val_true_labels)))[:5] + '  | ' +
+        ('%.2f' % (100 * evaluate_acc(                    tst_confidences,         tst_true_labels)))[:5] + ' / ' +
+        ('%.3f' % (      evaluate_nll(                    tst_confidences,         tst_true_labels)))[:5] + ' / ' +
+        ('%.3f' % (      evaluate_nll(temperature_scaling(tst_confidences, t_opt), tst_true_labels)))[:5] + '  | ' +
+        '\n'
+    )
